cd3c7d245495f698a201c12584b87fa788f4770f
ZiadNader1011 <202101425@pua.edu.eg>
2026-05-22 15:47:27 +0300

    modify vercel

--- todolist1-main/to do list modern company/backend/index.js
+++ todolist1-main/to do list modern company/backend/index.js
@@ -5,7 +5,11 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || "*", // هيسمح للفرونت إند الأونلاين بتاعك بس، ولو مش موجود هيفتح للكل كحماية ديفولت
+  methods: ["GET", "POST", "PATCH", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 // --- TASKS ---
@@ -252,8 +256,9 @@ app.delete("/employees/:id", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("🚀 Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5002; // سيقرأ البورت المكتوب في الـ .env أو بورت فيرسيل تلقائياً
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
 
 process.on("SIGINT", async () => {
